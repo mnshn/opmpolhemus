ab6d07971bf836fae6972b23f61a3784f68548b4
Paul de Lange <paul@saulx.com>
2019-12-04 18:22:18 +0100

    id on com works

--- opmpolhemus/handlers/looper.py
+++ opmpolhemus/handlers/looper.py
@@ -20,31 +20,35 @@ def diff_of_com(points):
         return np.inf
 
 
+def is_not_part(p_index, K_index, points, pcl, com_threshold):
+    return (
+        (p_index > 6) and
+        abs(np.linalg.norm(com(points) - pcl[K_index]) - diff_of_com(points)) >
+        com_threshold)
+
+
 def looper(pcl, start=8, double_click_threshold=10e-4):
     output = {}
     for i in range(0, OPMS):
         output[i] = {}
         point_index = 0
         j = 0
-        while point_index < 9:
-            points_so_far = list(map(lambda x: x[0], list(output[0].values())))
+        while point_index < 15:
+            unique_points_so_far = list(
+                map(lambda x: x[0], list(output[0].values())))
             K = start + (i + 1) * j
-            print(
-                np.linalg.norm(com(points_so_far) - pcl[K]) -
-                diff_of_com(points_so_far))
-            # print(
-            #     'away from com:',
-            #     np.linalg.norm(com(points_so_far) - pcl[K]) /
-            #     np.linalg.norm(com(points_so_far)))
-            # print('delta with last:', np.linalg.norm(pcl[K] - pcl[K - 1]))
-            for L in range(0, point_index):
-                if np.linalg.norm(pcl[K] -
-                                  output[i][L][0]) < double_click_threshold:
-                    output[i][L].append(pcl[K])
-                    j += 1
-                    break
+            if is_not_part(point_index, K, unique_points_so_far, pcl, 0.01):
+                break
             else:
-                output[i][point_index] = [pcl[K]]
-                j += 1
-                point_index += 1
+                for L in range(0, point_index):
+                    if np.linalg.norm(
+                            pcl[K] - output[i][L][0]) < double_click_threshold:
+                        output[i][L].append(pcl[K])
+                        j += 1
+                        break
+                else:
+                    output[i][point_index] = [pcl[K]]
+                    j += 1
+                    point_index += 1
+    print(output)
     return output
