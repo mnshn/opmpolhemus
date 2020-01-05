961986a86d4a9c5f29789794d12e3264d3eaffc8
Paul de Lange <paul@saulx.com>
2020-01-05 14:28:44 +0100

    testfile

--- opmpolhemus/main.py
+++ opmpolhemus/main.py
@@ -1,11 +1,15 @@
 from utils.parser import mat_parser
-
 from handler.coreg import CoReg
 
 test_file = '../db/proper02/point.txt'
-data = mat_parser(test_file)
 
-coreg = CoReg(data, 'top')
-coreg.show_coreg()
 
-# sensor coordinates are given by: coreg.sensors
+def coreg(data, frame_style, log_level=0):
+    parsed_data = mat_parser(data)
+    coreg_out = CoReg(parsed_data, frame_style)
+    if log_level == 1:
+        coreg_out.show_coreg()
+    return coreg_out.sensors
+
+
+coreg(test_file, 'top', log_level=1)
