f4da04d5de2c014fe2e67bc0663bed62e53b4bcd
Paul de Lange <paul@saulx.com>
2020-01-10 13:46:29 +0100

    renamed some things in cluster

--- opmpolhemus/cluster/clusters.py
+++ opmpolhemus/cluster/clusters.py
@@ -7,20 +7,22 @@ from opmpolhemus.helpers.com import diff_of_com
 
 
 def next_opm(frame,
-             unique_pen_click,
+             point,
              pcl_index,
-             points,
+             unique_points,
              turns,
              pcl,
              com_threshold=Constants.COM_THRESHOLD,
              distance_threshold=Constants.DISTANCE_THRESHOLD):
-    points = list(pcl[i] for i in points)
-    far_from_com = ((turns + 1) * unique_pen_click > frame.order - 1) and abs(
-        np.linalg.norm(com(points) - pcl[pcl_index]) -
-        diff_of_com(points)) > com_threshold
+    unique_points = list(pcl[i] for i in unique_points)
 
-    very_far_from_previous = (unique_pen_click > 0) and np.linalg.norm(
-        pcl[pcl_index] - pcl[pcl_index - 1]) > distance_threshold
+    far_from_com = ((turns + 1) * point > frame.order - 1) and abs(
+        np.linalg.norm(com(unique_points) - pcl[pcl_index]) -
+        diff_of_com(unique_points)) > com_threshold
+
+    very_far_from_previous = (
+        point > 0) and np.linalg.norm(pcl[pcl_index] -
+                                      pcl[pcl_index - 1]) > distance_threshold
 
     return (far_from_com or very_far_from_previous)
 
@@ -34,13 +36,13 @@ def cluster_opms(pcl,
     while pcl_index < len(pcl):
         opm_index += 1
         output[opm_index] = {}
-        unique_pen_click = 0
+        unique_point = 0
         turns = 0
         while turns < frame.turns and pcl_index < len(pcl):
             unique_points_so_far = list(x[0]
                                         for x in output[opm_index].values())
-            if next_opm(frame, unique_pen_click, pcl_index,
-                        unique_points_so_far, turns, pcl):
+            if next_opm(frame, unique_point, pcl_index, unique_points_so_far,
+                        turns, pcl):
                 break
             else:
                 for index, value in enumerate(unique_points_so_far):
@@ -48,12 +50,12 @@ def cluster_opms(pcl,
                                       pcl[value]) < double_click_threshold:
                         output[opm_index][index].append(pcl_index)
                         if (not value == pcl_index - 1):
-                            unique_pen_click += 1
+                            unique_point += 1
                         break
                 else:
-                    output[opm_index][unique_pen_click] = [pcl_index]
-                    unique_pen_click += 1
+                    output[opm_index][unique_point] = [pcl_index]
+                    unique_point += 1
             pcl_index += 1
-            if unique_pen_click == frame.order:
+            if unique_point == frame.order:
                 turns += 1
     return output
